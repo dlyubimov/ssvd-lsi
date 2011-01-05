f8b1f86453d15a5916e6ca74f98265a717386e30
Dmitriy Lyubimov <dlyubimov@inadco.com>
2011-01-04 16:24:22 -0800

    Efficiency improvements for BBt job (no more combiners).

--- core/src/main/java/org/apache/mahout/math/hadoop/stochasticsvd/BBtJob.java
+++ core/src/main/java/org/apache/mahout/math/hadoop/stochasticsvd/BBtJob.java
@@ -18,6 +18,7 @@
 package org.apache.mahout.math.hadoop.stochasticsvd;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -26,6 +27,7 @@ import org.apache.hadoop.io.SequenceFile.CompressionType;
 import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -35,78 +37,156 @@ import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.VectorWritable;
 
 /**
- * Compute B*Bt using simple fact that B*Bt = sum(outer prod ( B_(*i), (B_(*i)) ).
+ * Compute B*Bt using simple fact that B*Bt = sum(outer prod ( B_(*i), (B_(*i))
+ * ).
  * 
  * @author dmitriy
- *
+ * 
  */
 public class BBtJob {
 
     public static final String OUTPUT_BBt = "BBt";
-	
-    
-	public static void run ( Configuration conf, Path btPath, Path outputPath, 
-			int numReduceTasks )
-	throws IOException, ClassNotFoundException,InterruptedException { 
-		
-		Job job = new Job(conf);
-		job.setJobName("BBt-job");
+
+    public static void run(Configuration conf, Path btPath, Path outputPath,
+            int numReduceTasks) throws IOException, ClassNotFoundException,
+            InterruptedException {
+
+        Job job = new Job(conf);
+        job.setJobName("BBt-job");
         job.setJarByClass(BBtJob.class);
 
-	
-		// input 
-		job.setInputFormatClass(SequenceFileInputFormat.class);
-		FileInputFormat.setInputPaths(job, btPath);
-		
-		// map 
-		job.setMapOutputKeyClass(IntWritable.class);
-		job.setMapOutputValueClass(VectorWritable.class);
-		job.setMapperClass(BBtMapper.class);
-		
-		
-		// combiner and reducer
-		job.setReducerClass(BtJob.OuterProductReducer.class);
-		job.setCombinerClass(BtJob.OuterProductReducer.class);
-		job.setOutputKeyClass(IntWritable.class);
-		job.setOutputValueClass(VectorWritable.class);
-		
-		// output
-		job.setOutputFormatClass(SequenceFileOutputFormat.class);
-		FileOutputFormat.setOutputPath(job, outputPath);
-		SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
-		SequenceFileOutputFormat.setOutputCompressorClass(job, DefaultCodec.class);
+        // input
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        FileInputFormat.setInputPaths(job, btPath);
+
+        // map
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(VectorWritable.class);
+        job.setMapperClass(BBtMapper.class);
+        job.setReducerClass(BBtReducer.class);
+
+        // combiner and reducer
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(VectorWritable.class);
+
+        // output
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        SequenceFileOutputFormat.setOutputCompressionType(job,
+                CompressionType.BLOCK);
+        SequenceFileOutputFormat.setOutputCompressorClass(job,
+                DefaultCodec.class);
         job.getConfiguration().set("mapreduce.output.basename", OUTPUT_BBt);
-		
-		// run
-		job.submit();
-		job.waitForCompletion(false);
-		if ( ! job.isSuccessful()) throw new IOException ("BBt job failed.");
-		return;
-		
-	}
-	
-	public static class BBtMapper extends Mapper<IntWritable, VectorWritable, IntWritable, VectorWritable> {
 
-		private VectorWritable m_vw = new VectorWritable();
-		private IntWritable m_iw = new IntWritable();
-		private DenseVector  m_v;
-		double[] m_vRow;
-		
+        // run
+        job.submit();
+        job.waitForCompletion(false);
+        if (!job.isSuccessful())
+            throw new IOException("BBt job failed.");
+        return;
 
-		@Override
-		protected void map(IntWritable key, VectorWritable value,
-				Context context) throws IOException, InterruptedException {
-			Vector btVec=value.get();
-			int kp=btVec.size();
-			if ( m_v == null ) { m_v = new DenseVector(m_vRow=new double[kp],true); m_vw.set(m_v); }
-			for ( int i =0; i < kp; i++ ) { 
-			    // this approach should reduce GC churn rate
-			    double mul=btVec.getQuick(i);
-			    for ( int j =0; j < kp; j++ ) m_vRow[j] = mul*btVec.getQuick(j);
-			    m_iw.set(i);
-			    context.write(m_iw, m_vw);
-			}
-		}
-	}
-	
+    }
+
+    // actually, B*Bt matrix is small enough so that we don't need to rely on
+    // combiner.
+    public static class BBtMapper extends
+            Mapper<IntWritable, VectorWritable, IntWritable, VectorWritable> {
+
+        private VectorWritable m_vw = new VectorWritable();
+        private IntWritable m_iw = new IntWritable();
+        private UpperTriangular m_bbtPartial; // are all partial BBt products symmetrical as well? yes.
+
+        @Override
+        protected void map(IntWritable key, VectorWritable value,
+                Context context) throws IOException, InterruptedException {
+            Vector btVec = value.get();
+            int kp = btVec.size();
+            if (m_bbtPartial == null) {
+                m_bbtPartial=new UpperTriangular(kp);
+            }
+            for (int i = 0; i < kp; i++) {
+                // this approach should reduce GC churn rate
+                double mul = btVec.getQuick(i);
+                for (int j = i; j < kp; j++)
+                    m_bbtPartial.setQuick(i, j, m_bbtPartial.getQuick(i, j)+mul*btVec.getQuick(j));
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException,
+                InterruptedException {
+            if ( m_bbtPartial != null ) {
+                m_iw.set(context.getTaskAttemptID().getTaskID().getId());
+                m_vw.set(new DenseVector(m_bbtPartial.getData(), true));
+                context.write(m_iw, m_vw);
+            }
+            super.cleanup(context);
+        }
+    }
+    
+    public static class BBtReducer extends Reducer<IntWritable, VectorWritable, IntWritable, VectorWritable> {
+
+        private double[] m_accum;
+        
+        @Override
+        protected void cleanup(
+                Context context)
+                throws IOException, InterruptedException {
+            try { 
+                if ( m_accum != null ) 
+                    context.write(new IntWritable(),
+                            new VectorWritable(new DenseVector(m_accum,true))
+                            );
+            } finally {
+                super.cleanup(context);
+            }
+        }
+
+        @Override
+        protected void reduce(IntWritable iw, 
+                Iterable<VectorWritable> ivw,
+                Context ctx)
+                throws IOException, InterruptedException {
+            Iterator<VectorWritable> vwIter=ivw.iterator();
+            Vector bbtPartial=vwIter.next().get();
+            if ( m_accum == null ) { 
+                m_accum=new double[bbtPartial.size()];
+            }
+            do { 
+                for ( int i = 0; i < m_accum.length;i++) 
+                    m_accum[i]+=bbtPartial.getQuick(i);
+            } while ( vwIter.hasNext() && null != (bbtPartial=vwIter.next().get()));
+        } 
+        
+    }
+
+    // naive mapper.
+//    public static class BBtMapper extends
+//            Mapper<IntWritable, VectorWritable, IntWritable, VectorWritable> {
+//
+//        private VectorWritable m_vw = new VectorWritable();
+//        private IntWritable m_iw = new IntWritable();
+//        private DenseVector m_v;
+//        double[] m_vRow;
+//
+//        @Override
+//        protected void map(IntWritable key, VectorWritable value,
+//                Context context) throws IOException, InterruptedException {
+//            Vector btVec = value.get();
+//            int kp = btVec.size();
+//            if (m_v == null) {
+//                m_v = new DenseVector(m_vRow = new double[kp], true);
+//                m_vw.set(m_v);
+//            }
+//            for (int i = 0; i < kp; i++) {
+//                // this approach should reduce GC churn rate
+//                double mul = btVec.getQuick(i);
+//                for (int j = 0; j < kp; j++)
+//                    m_vRow[j] = mul * btVec.getQuick(j);
+//                m_iw.set(i);
+//                context.write(m_iw, m_vw);
+//            }
+//        }
+//    }
+
 }
