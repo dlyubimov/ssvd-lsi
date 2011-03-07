2c9051c8531925fea2abef8e9e79af9d084bd97a
Dmitriy Lyubimov <dlieu.7@gmail.com>
2011-03-06 22:43:38 -0800

    missed identation style issues in EigenSolverWrapper

--- math/src/main/java/org/apache/mahout/math/ssvd/EigenSolverWrapper.java
+++ math/src/main/java/org/apache/mahout/math/ssvd/EigenSolverWrapper.java
@@ -21,37 +21,44 @@ import org.apache.commons.math.linear.EigenDecompositionImpl;
 import org.apache.commons.math.linear.RealMatrix;
 
 /**
- * wraps appropriate eigen solver for BBt matrix. 
- * Can be either colt or apache commons math. <P>
+ * wraps appropriate eigen solver for BBt matrix. Can be either colt or apache
+ * commons math.
+ * <P>
  * 
- * At the moment it is apache commons math which 
- * is only in mahout-math dependencies. <P>
+ * At the moment it is apache commons math which is only in mahout-math
+ * dependencies.
+ * <P>
  * 
- * I will be happy to switch this to Colt eigensolver 
- * if it is proven reliable (i experience internal errors 
- * and unsorted singular values at some point).
+ * I will be happy to switch this to Colt eigensolver if it is proven reliable
+ * (i experience internal errors and unsorted singular values at some point).
  * 
  * But for now commons-math seems to be more reliable.
  * 
  * 
  */
 public class EigenSolverWrapper {
-    
-    private double[]    eigenvalues;
-    private double[][]  uHat;
 
-    public EigenSolverWrapper(double[][] bbt ) {
-        super();
-        int dim=bbt.length;
-        EigenDecompositionImpl evd2=new EigenDecompositionImpl(new Array2DRowRealMatrix(bbt),0);
-        eigenvalues=evd2.getRealEigenvalues();
-        RealMatrix uHatrm= evd2.getV();
-        uHat = new double[dim][];
-        for ( int i = 0; i < dim; i++ ) uHat [i]=uHatrm.getRow(i);
-    }
-    
-    public double[][] getUHat() { return uHat; }
-    public double[] getEigenValues() { return eigenvalues; } 
-    
-    
+  private double[] eigenvalues;
+  private double[][] uHat;
+
+  public EigenSolverWrapper(double[][] bbt) {
+    super();
+    int dim = bbt.length;
+    EigenDecompositionImpl evd2 = new EigenDecompositionImpl(
+        new Array2DRowRealMatrix(bbt), 0);
+    eigenvalues = evd2.getRealEigenvalues();
+    RealMatrix uHatrm = evd2.getV();
+    uHat = new double[dim][];
+    for (int i = 0; i < dim; i++)
+      uHat[i] = uHatrm.getRow(i);
+  }
+
+  public double[][] getUHat() {
+    return uHat;
+  }
+
+  public double[] getEigenValues() {
+    return eigenvalues;
+  }
+
 }
